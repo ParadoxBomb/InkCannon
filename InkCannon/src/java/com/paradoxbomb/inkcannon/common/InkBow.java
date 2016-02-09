660df7bb22cfa99320c245a7742d2e7e96c6abae
David <zaneqlster@gmail.com>
2016-02-09 01:13:14 -0700

    added functionality to inkBow, created EntityInkArrow

--- InkCannon/src/java/com/paradoxbomb/inkcannon/common/InkBow.java
+++ InkCannon/src/java/com/paradoxbomb/inkcannon/common/InkBow.java
@@ -4,6 +4,7 @@ package com.paradoxbomb.inkcannon.common;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -20,6 +21,10 @@ public class InkBow extends Item
 	
 	public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn)
 	{
+		if(!worldIn.isRemote)
+		{
+			worldIn.spawnEntityInWorld(new EntityArrow(worldIn, playerIn, 15));
+		}
 		
 		return itemStackIn;
 	}
